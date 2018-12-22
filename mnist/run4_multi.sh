d491472bba4f9650e536388fb09155bf066e6480
OMRON's teoroda <Hao.Tang201812@sinicx.com>
2018-12-23 00:53:08 +0900

    Add multi4 related files

---
+++ mnist/run4_multi.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+CUDA_VISIBLE_DEVICES=1 python3 my_multi_tune4.py &
+sleep 5
+#CUDA_VISIBLE_DEVICES=4 python3 my_multi_tune4.py &
+#sleep 5
+#CUDA_VISIBLE_DEVICES=4 python3 my_multi_tune4.py &
+#sleep 5
+#CUDA_VISIBLE_DEVICES=4 python3 my_multi_tune4.py &
+#sleep 5
+#CUDA_VISIBLE_DEVICES=5 python3 my_multi_tune4.py &
+#sleep 5
+#CUDA_VISIBLE_DEVICES=5 python3 my_multi_tune4.py &
+#sleep 5
+#CUDA_VISIBLE_DEVICES=5 python3 my_multi_tune4.py &
+#sleep 5
+CUDA_VISIBLE_DEVICES=6 python3 my_multi_tune4.py &
+sleep 5
+CUDA_VISIBLE_DEVICES=6 python3 my_multi_tune4.py &
+sleep 5
+CUDA_VISIBLE_DEVICES=6 python3 my_multi_tune4.py &
+sleep 5
+#CUDA_VISIBLE_DEVICES=7 python3 my_multi_tune4.py &
+#sleep 5
+#CUDA_VISIBLE_DEVICES=7 python3 my_multi_tune4.py &
+#sleep 5
+#CUDA_VISIBLE_DEVICES=8 python3 my_multi_tune4.py &
+#sleep 5
+CUDA_VISIBLE_DEVICES="" python3 my_multi_tune4.py &
+sleep 5
+CUDA_VISIBLE_DEVICES="" python3 my_multi_tune4.py &
+sleep 5
+CUDA_VISIBLE_DEVICES="" python3 my_multi_tune4.py &
+#sleep 5
+#CUDA_VISIBLE_DEVICES="" python3 my_multi_tune4.py &
+#sleep 5
