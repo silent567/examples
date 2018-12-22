c6155609c89549eedd52b4838a1cd56bee5376b6
OMRON's teoroda <Hao.Tang201812@sinicx.com>
2018-12-22 14:24:59 +0900

    Add multi_3

---
+++ mnist/run3_multi.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#CUDA_VISIBLE_DEVICES=4 python3 my_multi_tune3.py &
+#sleep 5
+#CUDA_VISIBLE_DEVICES=4 python3 my_multi_tune3.py &
+#sleep 5
+#CUDA_VISIBLE_DEVICES=4 python3 my_multi_tune3.py &
+#sleep 5
+CUDA_VISIBLE_DEVICES=5 python3 my_multi_tune3.py &
+sleep 5
+CUDA_VISIBLE_DEVICES=5 python3 my_multi_tune3.py &
+sleep 5
+CUDA_VISIBLE_DEVICES=5 python3 my_multi_tune3.py &
+sleep 5
+CUDA_VISIBLE_DEVICES=6 python3 my_multi_tune3.py &
+sleep 5
+CUDA_VISIBLE_DEVICES=6 python3 my_multi_tune3.py &
+sleep 5
+CUDA_VISIBLE_DEVICES=6 python3 my_multi_tune3.py &
+sleep 5
+CUDA_VISIBLE_DEVICES=7 python3 my_multi_tune3.py &
+sleep 5
+CUDA_VISIBLE_DEVICES=7 python3 my_multi_tune3.py &
+sleep 5
+CUDA_VISIBLE_DEVICES=8 python3 my_multi_tune3.py &
