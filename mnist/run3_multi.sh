1273c8b302a1b42a7f86fd00913cf58bc6835d36
OMRON's teoroda <Hao.Tang201812@sinicx.com>
2018-12-22 14:31:11 +0900

    Add some CPU processes

--- mnist/run3_multi.sh
+++ mnist/run3_multi.sh
@@ -6,20 +6,29 @@
 #sleep 5
 #CUDA_VISIBLE_DEVICES=4 python3 my_multi_tune3.py &
 #sleep 5
-CUDA_VISIBLE_DEVICES=5 python3 my_multi_tune3.py &
+#CUDA_VISIBLE_DEVICES=5 python3 my_multi_tune3.py &
+#sleep 5
+#CUDA_VISIBLE_DEVICES=5 python3 my_multi_tune3.py &
+#sleep 5
+#CUDA_VISIBLE_DEVICES=5 python3 my_multi_tune3.py &
+#sleep 5
+#CUDA_VISIBLE_DEVICES=6 python3 my_multi_tune3.py &
+#sleep 5
+#CUDA_VISIBLE_DEVICES=6 python3 my_multi_tune3.py &
+#sleep 5
+#CUDA_VISIBLE_DEVICES=6 python3 my_multi_tune3.py &
+#sleep 5
+#CUDA_VISIBLE_DEVICES=7 python3 my_multi_tune3.py &
+#sleep 5
+#CUDA_VISIBLE_DEVICES=7 python3 my_multi_tune3.py &
+#sleep 5
+#CUDA_VISIBLE_DEVICES=8 python3 my_multi_tune3.py &
+#sleep 5
+CUDA_VISIBLE_DEVICES="" python3 my_multi_tune3.py &
 sleep 5
-CUDA_VISIBLE_DEVICES=5 python3 my_multi_tune3.py &
+CUDA_VISIBLE_DEVICES="" python3 my_multi_tune3.py &
 sleep 5
-CUDA_VISIBLE_DEVICES=5 python3 my_multi_tune3.py &
+CUDA_VISIBLE_DEVICES="" python3 my_multi_tune3.py &
 sleep 5
-CUDA_VISIBLE_DEVICES=6 python3 my_multi_tune3.py &
+CUDA_VISIBLE_DEVICES="" python3 my_multi_tune3.py &
 sleep 5
-CUDA_VISIBLE_DEVICES=6 python3 my_multi_tune3.py &
-sleep 5
-CUDA_VISIBLE_DEVICES=6 python3 my_multi_tune3.py &
-sleep 5
-CUDA_VISIBLE_DEVICES=7 python3 my_multi_tune3.py &
-sleep 5
-CUDA_VISIBLE_DEVICES=7 python3 my_multi_tune3.py &
-sleep 5
-CUDA_VISIBLE_DEVICES=8 python3 my_multi_tune3.py &
