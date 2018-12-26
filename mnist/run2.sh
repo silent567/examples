079232dc78abaf30b9bda5f1f347544d9412d1cd
OMRON's teoroda <Hao.Tang201812@sinicx.com>
2018-12-26 13:32:22 +0900

    change run2.sh

--- mnist/run2.sh
+++ mnist/run2.sh
@@ -6,22 +6,22 @@ CUDA_VISIBLE_DEVICES=5 python3 my_tune2.py &
 sleep 5
 CUDA_VISIBLE_DEVICES=5 python3 my_tune2.py &
 sleep 5
-CUDA_VISIBLE_DEVICES=6 python3 my_tune2.py &
-sleep 5
-CUDA_VISIBLE_DEVICES=6 python3 my_tune2.py &
-sleep 5
-CUDA_VISIBLE_DEVICES=6 python3 my_tune2.py &
-sleep 5
-CUDA_VISIBLE_DEVICES=7 python3 my_tune2.py &
-sleep 5
-CUDA_VISIBLE_DEVICES=7 python3 my_tune2.py &
-sleep 5
-CUDA_VISIBLE_DEVICES=7 python3 my_tune2.py &
+#CUDA_VISIBLE_DEVICES=6 python3 my_tune2.py &
+#sleep 5
+#CUDA_VISIBLE_DEVICES=6 python3 my_tune2.py &
+#sleep 5
+#CUDA_VISIBLE_DEVICES=6 python3 my_tune2.py &
 #sleep 5
 #CUDA_VISIBLE_DEVICES=7 python3 my_tune2.py &
-sleep 5
-CUDA_VISIBLE_DEVICES="" python3 my_tune2.py &
-sleep 5
-CUDA_VISIBLE_DEVICES="" python3 my_tune2.py &
-sleep 5
-CUDA_VISIBLE_DEVICES="" python3 my_tune2.py &
+#sleep 5
+#CUDA_VISIBLE_DEVICES=7 python3 my_tune2.py &
+#sleep 5
+#CUDA_VISIBLE_DEVICES=7 python3 my_tune2.py &
+##sleep 5
+##CUDA_VISIBLE_DEVICES=7 python3 my_tune2.py &
+#sleep 5
+#CUDA_VISIBLE_DEVICES="" python3 my_tune2.py &
+#sleep 5
+#CUDA_VISIBLE_DEVICES="" python3 my_tune2.py &
+#sleep 5
+#CUDA_VISIBLE_DEVICES="" python3 my_tune2.py &
